daf4ec34a08e1583900a3cdae7bf9d0d95cd7ba9
Milton J. Neto <miltonjacomini@gmail.com>
2017-06-17 12:56:42 -0300

    Novas alterações

--- ceep/js/scripts.js
+++ ceep/js/scripts.js
@@ -1,13 +1,34 @@
-function mudaLayout(){
 
-  var listaMural = document.querySelector(".mural").classList;
+document.querySelector('#mudaLayout').addEventListener('click', function mudaLayout(){
 
-    if(listaMural.contains('mural-linha')){
-        listaMural.remove("mural-linha")
-    }else{
-        listaMural.add("mural-linha")
+        var mural = document.querySelector('.mural');
+
+        mural.classList.toggle('mural--linhas');
+
+        if(mural.classList.contains('mural--linhas')){
+            this.textContent = 'Colunas';
+        } else {
+            this.textContent = 'Linhas';
+        }
+
     }
+);
 
+
+var botoes = document.querySelectorAll('.opcoesDoCartao-remove');
+
+for(var i=0; i < botoes.length; i++ ){
+    botoes[i].addEventListener("click", removeCartao);
 }
 
-document.querySelector('input').addEventListener('click', mudaLayout)
+function removeCartao(){
+
+    var cartao = document.querySelector('#cartao_'+ this.dataset.ref);
+
+    cartao.classList.add('cartao--some');
+
+    setTimeout(function(){
+        cartao.remove();
+    }, 400)
+
+}
